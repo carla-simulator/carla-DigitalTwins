059bde9b6a98f006de081dd7fbca3717b4fd181f
Marcel Pi <169088301+MarcelPiNacy-CVC@users.noreply.github.com>
2025-06-23 18:39:16 +0200

    Add back opendrive generation params and fix log categories.

--- Source/CarlaDigitalTwinsTool/Public/Generation/OpenDriveToMap.h
+++ Source/CarlaDigitalTwinsTool/Public/Generation/OpenDriveToMap.h
@@ -11,7 +11,7 @@
 #include "Components/HierarchicalInstancedStaticMeshComponent.h"
 #include <Carla/Road/RoadMap.h>
 #include <boost/optional.hpp>
-
+#include "Generation/OpenDriveFileGenerationParameters.h"
 #include "OpenDriveToMap.generated.h"
 
 
@@ -131,6 +131,9 @@ public:
 
   UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Defaults")
   UMaterialInstance* DefaultLandscapeMaterial;
+  
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Parameters")
+  FOpenDriveFileGenerationParameters OpenDriveGenParams;
 
   UPROPERTY( EditAnywhere, BlueprintReadWrite, Category="Settings" )
   float DistanceBetweenTrees = 50.0f;
